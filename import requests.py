c2cc02d02b93d037f48082cabfe9f7170d97882a
sachin573 <sachin.katoch050@gmail.com>
2022-06-03 00:21:27 -0400

    first

---
+++ import requests.py
@@ -0,0 +1,8 @@
+import requests
+city ='New Delhi'
+api_url = 'https://api.api-ninjas.com/v1/airquality?city={}'.format(city)
+response = requests.get(api_url, headers={'X-Api-Key': 'P3bNLRZlg24B7Ny7GAX+sA==wHDzHG8vClUw9ZWl'})
+if response.status_code == requests.codes.ok:
+    print(response.text)
+else:
+    print("Error:", response.status_code, response.text)
